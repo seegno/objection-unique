f1137bc19f2603be85a54edca79ce6efb01bc94e
Benjamin Marquis <44323490+BjMrq@users.noreply.github.com>
2020-09-08 07:57:56 -0400

    Update index.d.ts
    - Made Options, identifiers and fields required
    - Extended types accepted by identifiers and fields to allow for string or array of strings
    - Changed the returned value to be the model itself and not a Constructor of it

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,8 @@
 declare module 'objection-unique' {
   import { Model } from 'objection';
 
-  type Constructor<T = {}> = new (...args: any[]) => T;
-
-  export default function (options?: {
-    identifiers?: string[];
-    fields?: string[];
-  }): <T extends typeof Model>(model: T) => T & Constructor<T>
+  export default function (options: {
+    identifiers: Array<string | string[]>;
+    fields: Array<string | string[]>;
+  }): <T extends typeof Model>(model: T) => T
 }
