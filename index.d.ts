009aba48441928b068d7e9ecdae5914871956aaa
Benjamin Marquis <44323490+BjMrq@users.noreply.github.com>
2020-10-08 14:16:10 -0400

    Update index.d.ts
    Made identifier optional

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module 'objection-unique' {
   import { Model } from 'objection';
 
   export default function (options: {
-    identifiers: Array<string | string[]>;
+    identifiers?: Array<string | string[]>;
     fields: Array<string | string[]>;
   }): <T extends typeof Model>(model: T) => T
 }
